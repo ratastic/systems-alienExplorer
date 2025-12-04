063135904b8efecc95697332385ebfae008eac1c
Drinomino <pamelamckeown2004@gmail.com>
2025-12-03 21:43:09 -0500

    butoons

---
+++ alien-exploration/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
