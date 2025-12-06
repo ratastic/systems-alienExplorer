94ee93bf9a587bb56447477b8057addfe670969b
vega jafri <giantwhitedeer@gmail.com>
2025-12-06 05:51:53 -0500

    cameamovememnt

--- alien-exploration/Assets/Scenes/seed01/CameraFollow.cs
+++ alien-exploration/Assets/Scenes/seed01/CameraFollow.cs
@@ -5,16 +5,35 @@ using System.Collections.Generic;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    private Vector3 offset;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        offset = transform.position - target.position;
-    }
+    public float distance = 5f; // how far away from the target
+    public float mouseSensitivity = 5f;
+    public float minYAngle = 10f; // clamp vertical angle
+    public float maxYAngle = 60f; // clamp horizontal angle
 
-    // Update is called once per frame
-    void Update()
+    private float yaw = 0f; // rotation around Y axis ; left right movement
+    private float pitch = 20f; // rotation around X axis ; up down movement
+
+    public float viewOffset = 5f; // offset so that target object isn't smack in the center
+
+    void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, target.position.z + offset.z);
+        if (!target) return;
+
+        // read mouse movement
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        // clamp rotation around x axis so camera doesn't flip
+        pitch = Mathf.Clamp(pitch, minYAngle, maxYAngle);
+
+        // calculate rotation
+        Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
+
+        // defines camera position in relation to the target object
+        Vector3 desiredPos = target.position - (rotation * Vector3.forward * distance);
+        transform.position = desiredPos;
+
+        // always look at target object 
+        transform.LookAt(target.position + Vector3.up * viewOffset);
     }
 }
